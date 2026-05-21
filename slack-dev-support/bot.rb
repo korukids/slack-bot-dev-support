7d1cf29a42996ac7c81f109832815f836d8fe4d8
Rob Jackson <rob.jackson@korukids.co.uk>
2026-05-21 12:56:55 +0100

    Refactor: share assign_for between dev-support and prod-design schedulers

--- slack-dev-support/bot.rb
+++ slack-dev-support/bot.rb
@@ -28,35 +28,29 @@ module SlackDevSupport
     end
   end
 
-  def self.assign
-    Redis.current.rpoplpush("#{DEVELOPER_CHANNEL}_users", "#{DEVELOPER_CHANNEL}_users")
+  def self.assign_for(redis_channel:, announce_channel:, message_template:)
+    # Daily rotation: tail becomes head (everyone shifts down one position).
+    Redis.current.rpoplpush("#{redis_channel}_users", "#{redis_channel}_users")
 
-    yesterdays_not_applicable = Redis.current.lrange("#{DEVELOPER_CHANNEL}_not_applicable", 0, 200)
-
+    # Bring yesterday's _not_applicable bucket back into the active queue.
+    yesterdays_not_applicable = Redis.current.lrange("#{redis_channel}_not_applicable", 0, 200)
     yesterdays_not_applicable.each do |user|
-      Redis.current.rpush("#{DEVELOPER_CHANNEL}_users", user)
+      Redis.current.rpush("#{redis_channel}_users", user)
     end
+    Redis.current.del("#{redis_channel}_not_applicable")
 
-    Redis.current.del("#{DEVELOPER_CHANNEL}_not_applicable")
+    selected = Redis.current.lrange("#{redis_channel}_users", 0, 200).last
+    $slack_client.chat_postMessage(channel: announce_channel,
+                                   text: format(message_template, user: "<@#{selected}>"))
+  end
 
-    selected = Redis.current.lrange("#{DEVELOPER_CHANNEL}_users", 0, 200).last
-
-    $slack_client.chat_postMessage(channel: $channel, text: "<@#{selected}> is on dev support today!")
+  def self.assign
+    assign_for(redis_channel: DEVELOPER_CHANNEL, announce_channel: $channel,
+               message_template: '%<user>s is on dev support today!')
   end
 
   def self.assign_prod_design
-    Redis.current.rpoplpush("#{PRODUCT_DESIGN_CHANNEL}_users", "#{PRODUCT_DESIGN_CHANNEL}_users")
-
-    yesterdays_not_applicable = Redis.current.lrange("#{PRODUCT_DESIGN_CHANNEL}_not_applicable", 0, 200)
-
-    yesterdays_not_applicable.each do |user|
-      Redis.current.rpush("#{PRODUCT_DESIGN_CHANNEL}_users", user)
-    end
-
-    Redis.current.del("#{PRODUCT_DESIGN_CHANNEL}_not_applicable")
-
-    selected = Redis.current.lrange("#{PRODUCT_DESIGN_CHANNEL}_users", 0, 200).last
-
-    $slack_client.chat_postMessage(channel: $prod_design_channel, text: "<@#{selected}> is your chair today!")
+    assign_for(redis_channel: PRODUCT_DESIGN_CHANNEL, announce_channel: $prod_design_channel,
+               message_template: '%<user>s is your chair today!')
   end
 end
